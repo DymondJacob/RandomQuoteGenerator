3100655dae75d749e9dd2570078c3c8b93bf9e0b
JacobDymond <devdymond@gmail.com>
2018-05-22 21:47:18 -0500

    Last Final Edits
    Last final edit touch ups, fixed some comments, and reviewed everything to make sure it was good to go. Everything seems to work perfect

--- js/script.js
+++ js/script.js
@@ -1,59 +1,52 @@
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
 // Start of Code
 
-// JS Button click to execute printQuote
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-
-// strict (Good to use)
-
-
-'use strict'
-
-// The array that will contain the Quotes
+// This array that will contain the Quotes for the random quote generator
 var quotes = [];
 
-// Adding First quote as an object in the curly braces
+// Adding the First quote as an object
 quotes[0] = {quote: "Moral indignation is jealousy with a halo.",
              source: "H.G. Wells, ",
              category: " Literature"};
-// Adding Second quote as an object in curly braces
+// Adding the Second quote as an object
 quotes[1] = {quote: "Be yourself; everyone else is already taken.",
              source: "Oscar Wilde, ",
              category: " Literature"};
-//Adding Third quote as an object in the curly braces
+// Adding the Third quote as an object
 quotes[2] = {quote: "So many books, so little time.",
              source: "Frank Zappa, ",
              category: " Humor"};
-//Adding Fourth quote an object in the curly braces
+// Adding the Fourth quote an object
 quotes[3] = {quote: "A room without books is like a body without a soul",
              source: "Marcus Tullius Cicero, ",
              category: " Soul"};
-//Adding Fifth quote as an object in the curly braces
+// Adding the Fifth quote as an object
 quotes[4] = {quote: "You only live once, but if you do it right, once is enough.",
              source: "Mae West, ",
              category: " Life"};
-//Adding sixth quote as an object in the curly braces
+// Adding the sixth quote as an object in the curly braces
 quotes[5] = {quote: "Be the change that you wish to see in the world.",
              source: "Mahatma Gandhi, ",
              category: " Philosophy"};
-//Adding seventh quote as an object in the curly braces
+// Adding the seventh quote as an object in the curly braces
 quotes[6] = {quote: "If you tell the truth, you don't have to remember anything.",
              source: "Mark Twain, ",
              category: " Memory"};
-//Adding eigth quote as an object in the curly braces
+// Adding the eigth quote as an object in the curly braces
 quotes[7] = {quote: "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.",
              source: "Martin Luther King Jr., ",
              category: " Love"};
 
-// For debugging purposes, this code will log the quotes array to the console.
+//  I add this here for debugging purposes, the code below logs the quotes array
 console.log(quotes);
 
 // The number of times that printQuote needs to change through before all quotes are used
-var times = quotes.length
+var quoteTimes = quotes.length
 
-// This is another global array which will have the quote objects that have already been viewed once
+// Global array which will have the quote objects that have already been viewed once
 var quotesOther = [];
 
 function getRandomQuote (a,b, number) {
@@ -62,27 +55,27 @@ if (b.length < number) {
   // Picks random number based on quote array element length
   var num = Math.floor(Math.random() * a.length);
   //Move it out of the quotes array and then assign it to the name variable
-  var ou = a.splice(num,1);
+  var ad = a.splice(num,1);
   // this pushes the ou variable into the other array
-  b.push(ou[0]);
+  b.push(ad[0]);
   // Returns the quote object from the function, making sure none are duplicated until all are used
-  return ou[0];
+  return ad[0];
 }
 // When the other array is then full
 else {
   //picks a random number that is based on the length of the other array, which is full
-  var numb = Math.floor(Math.random() * b.length);
+  var number = Math.floor(Math.random() * b.length);
   // This assigns the quote to a variable based upon a random number that is selected
-  var qub = b[numb];
-  // then returns he quote
-  return qub;
+  var abc = b[number];
+  // then returns the quote
+  return abc;
   }
 };
 
 // Defines the printQuote function
 function printQuote() {
 // Uses the getRandomQuote function to get a random quote object from the auotes array
-var randomQuote = getRandomQuote(quotes, quotesOther, times);
+var randomQuote = getRandomQuote(quotes, quotesOther, quoteTimes);
 //Debugging, console.log();
 console.log(quotes);
 //Debugging
@@ -130,20 +123,20 @@ function getRandomColourBackground(){
   //set body background color
   document.body.style.backgroundColor = hexadec;
 }
-//random colour generator for button background color, makes it look nicer
+// random colour generator for button background color, it will make it look nicer, an extra touch
 function getRandomColourbutton(){
-  //Array of hexadecimal characters
+  //Array of the hexadecimal characters
   var hexadecscale = "0123456789ABCDEF".split('');
-  // hexidecimal String
+  // the hexidecimal string
   var hexadec = '#';
   var i = 0;
   while (i<6) {
     hexadec += hexadecscale[Math.floor(Math.random() * 16)];
     i += 1;
   }
-  //console.log the color to make sure that it is working
+  // console.log the color to make sure that it is working
   console.log(hexadec);
-  //sets the background colour
+  // sets the background colour
   document.getElementById("loadQuote").style.backgroundColor = hexadec;
   }
 
@@ -153,7 +146,7 @@ getRandomColourBackground();
 getRandomColourButton();
 }
 
-//call printQuote function upon initial page load
+//calls printQuote function when the page is first loaded
 printQuote();
-// call function after set interval of time which is 10 seconds
-setInterval('printQuote()', 10000);
+// call function after set interval of time which is 30 seconds
+setInterval('printQuote()', 30000);
